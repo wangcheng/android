370dbb6b3cf58b6a82f078fccdc0770020fd8e5e
Cheng Wang <wangcheng678@gmail.com>
2022-08-08 00:56:30 +0800

    add

--- lockscreen/src/main/java/io/github/wangcheng/lockscreen/LockScreenAccessibilityService.java
+++ lockscreen/src/main/java/io/github/wangcheng/lockscreen/LockScreenAccessibilityService.java
@@ -1,7 +1,11 @@
 package io.github.wangcheng.lockscreen;
 
 import android.accessibilityservice.AccessibilityService;
+import android.content.Context;
 import android.content.Intent;
+import android.os.Build;
+import android.os.VibrationEffect;
+import android.os.Vibrator;
 import android.provider.Settings;
 import android.view.accessibility.AccessibilityEvent;
 
@@ -18,26 +22,27 @@ public class LockScreenAccessibilityService extends AccessibilityService {
     public int onStartCommand(Intent intent, int flags, int startId) {
         String action = intent.getAction();
         if (action != null && action.equals(MainActivity.ACTION_LOCK_SCREEN)) {
-//            if (!performGlobalAction(GLOBAL_ACTION_LOCK_SCREEN)) {
-//                performVibration();
-//            } else {
-            openSettings();
-//            }
+            boolean isSuccess = performGlobalAction(GLOBAL_ACTION_LOCK_SCREEN);
+            if (isSuccess) {
+                performVibration();
+            } else {
+                openSettings();
+            }
         }
         return super.onStartCommand(intent, flags, startId);
     }
 
-//    private void performVibration() {
-//        Vibrator vibrator = (Vibrator) getSystemService(Context.VIBRATOR_SERVICE);
-//
-//        if (vibrator == null) return;
-//
-//        VibrationEffect effect = android.os.Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q
-//                ? VibrationEffect.createPredefined(VibrationEffect.EFFECT_TICK)
-//                : VibrationEffect.createOneShot(5, VibrationEffect.DEFAULT_AMPLITUDE);
-//
-//        vibrator.vibrate(effect);
-//    }
+    private void performVibration() {
+        Vibrator vibrator = (Vibrator) getSystemService(Context.VIBRATOR_SERVICE);
+
+        if (vibrator == null) return;
+
+        VibrationEffect effect = Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q
+                ? VibrationEffect.createPredefined(VibrationEffect.EFFECT_TICK)
+                : VibrationEffect.createOneShot(5, VibrationEffect.DEFAULT_AMPLITUDE);
+
+        vibrator.vibrate(effect);
+    }
 
     private void openSettings() {
         Intent goToSettings = new Intent(Settings.ACTION_ACCESSIBILITY_SETTINGS);
