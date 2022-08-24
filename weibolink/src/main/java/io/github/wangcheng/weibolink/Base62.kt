4144bdc3a3ab35a83d41991040d50062ba130c1d
Cheng Wang <wangcheng678@gmail.com>
2022-08-25 03:15:08 +0800

    diktatFix

--- weibolink/src/main/java/io/github/wangcheng/weibolink/Base62.kt
+++ weibolink/src/main/java/io/github/wangcheng/weibolink/Base62.kt
@@ -1,6 +1,5 @@
 package io.github.wangcheng.weibolink
 
-import kotlin.math.floor
 import kotlin.math.pow
 
 class Base62 {
@@ -9,46 +8,42 @@ class Base62 {
 
         // 62进制值转换为10进制
         private fun decode(inputStr: String): String {
-            var i10 = 0
+            var int10 = 0
             for (i in inputStr.indices) {
-                val n = inputStr.length - i - 1
-                val s = inputStr[i]
-                i10 += DICT.indexOf(s) * 62F.pow(n).toInt()
+                val powerNumber = inputStr.length - i - 1
+                val char = inputStr[i]
+                int10 += DICT.indexOf(char) * 62F.pow(powerNumber).toInt()
             }
-            return i10.toString()
+            return int10.toString()
         }
 
         // 10进制值转换为62进制
         private fun encode(str10: String): String {
-            var int10 = str10.toInt(10)
-            var s62 = ""
-            var r: Int
-            while (int10 != 0 && s62.length < 100) {
-                r = int10 % 62
-                s62 = DICT[r] + s62
-                int10 = floor(int10 / 62F).toInt()
+            var int10 = str10.toInt()
+            var str62 = ""
+            while (int10 != 0 && str62.length < 100) {
+                str62 = DICT[int10 % 62] + str62
+                int10 = int10.floorDiv(62)
             }
-            return s62
+            return str62
         }
 
-        private fun chunkString(str: String, size: Int): List<String> {
-            return str.reversed().chunked(size).map { it.reversed() }.reversed()
-        }
+        private fun chunkString(str: String, size: Int): List<String> =
+            str.reversed()
+                .chunked(size)
+                .map { it.reversed() }
+                .reversed()
 
-        fun url2mid(str: String): String {
-            return chunkString(str, 4).joinToString(
-                separator = "",
-                transform = { decode(it).padStart(7, '0') }
-            ).trimStart('0')
-        }
+        fun url2mid(str: String): String = chunkString(str, 4).joinToString(
+            separator = "",
+            transform = { decode(it).padStart(7, '0') },
+        ).trimStart('0')
 
-        fun mid2url(str: String): String {
-            return chunkString(str, 7).joinToString(
-                separator = "",
-                transform = {
-                    encode(it).padStart(4, '0')
-                }
-            ).trimStart('0')
-        }
+        fun mid2url(str: String): String = chunkString(str, 7).joinToString(
+            separator = "",
+            transform = {
+                encode(it).padStart(4, '0')
+            },
+        ).trimStart('0')
     }
 }
--- weibolink/src/main/java/io/github/wangcheng/weibolink/WeiboLinkUtils.kt
+++ weibolink/src/main/java/io/github/wangcheng/weibolink/WeiboLinkUtils.kt
@@ -3,23 +3,17 @@ package io.github.wangcheng.weibolink
 class WeiboLinkUtils {
     companion object {
         const val TAG = "WeiboLinkUtils"
-
         private const val WEIBO_QINGXIANG_PATTERN =
             "https?://share\\.api\\.weibo\\.cn/share/(\\d+),(\\d+).html.*"
-
         private const val WEIBO_INTL_WITH_ID_PATTERN =
             "https?://weibointl\\.api\\.weibo\\.cn/share/\\d+.html\\?weibo_id=(\\d+)"
-
         private const val WEIBO_CN_PATTERN =
             "https?://m\\.weibo\\.cn/.+/(\\d+)"
 
-        fun createWeiboComUrl(userId: String, mId: String): String {
-            return "https://weibo.com/${userId}/${Base62.mid2url(mId)}"
-        }
+        fun createWeiboComUrl(userId: String, id: String): String =
+            "https://weibo.com/$userId/${Base62.mid2url(id)}"
 
-        fun createWeiboCnUrl(mId: String): String {
-            return "https://m.weibo.cn/status/${mId}"
-        }
+        fun createWeiboCnUrl(id: String): String = "https://m.weibo.cn/status/$id"
 
         fun getWeiboIdFromWeiboQingxiang(url: String): String? {
             val reg = Regex(WEIBO_QINGXIANG_PATTERN)
@@ -45,16 +39,8 @@ class WeiboLinkUtils {
             }
         }
 
-        fun getWeiboIdFromUrl(url: String): String? {
-            var weiboId: String?
-            weiboId = getWeiboIdFromWeiboCn(url)
-            if (weiboId == null) {
-                weiboId = getWeiboIdFromWeiboQingxiang(url)
-            }
-            if (weiboId == null) {
-                weiboId = getWeiboIdFromWeiboIntlWithIdQuery(url)
-            }
-            return weiboId
-        }
+        fun getWeiboIdFromUrl(url: String): String? =
+            getWeiboIdFromWeiboCn(url) ?: getWeiboIdFromWeiboQingxiang(url)
+                ?: getWeiboIdFromWeiboIntlWithIdQuery(url)
     }
 }
